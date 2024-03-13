faa646cf729af8498b359eb0a0894dd64d811c01
a1830345 <jeff.chen1805@gmail.com>
2024-03-13 16:56:03 +1030

    better code

--- task1.js
+++ task1.js
@@ -1,15 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Select the element containing the text "THIS TEXT HERE"
-    var textElement = document.getElementById('current_time');
-
-    // Write a function to modify the contents of this element to display the current date and time
-    function displayDateTime() {
-        var currentDate = new Date();
-        var dateTimeString = currentDate.toLocaleString(); // Get date and time in localized format
-        textElement.innerText = dateTimeString; // Update the text content of the element
-    }
-
-    // Modify the button to run this function when clicked
-    var button = document.querySelector('button');
-    button.addEventListener('click', displayDateTime);
-});
+function date_class(){
+    document.getElementById('current_time').innerText = new Date();
+}
