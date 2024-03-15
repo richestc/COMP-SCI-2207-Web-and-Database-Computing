804cb1d97315d1d258601db63ac99ad36d194f25
a1830345 <jeff.chen1805@gmail.com>
2024-03-15 12:28:58 +1030

    3.1

---
+++ task3.js
@@ -0,0 +1,6 @@
+//section 3.1
+function mouse_pass_counter(){
+    var count = document.getElementById('mcount').innerHTML;
+    count++;
+    document.getElementById('mcount').innerHTML = count;
+}   
