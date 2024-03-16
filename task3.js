2e2483b49e5efa1bcef324d3b3a424d7e59d1988
a1830345 <jeff.chen1805@gmail.com>
2024-03-16 17:09:24 +1030

    3.4

--- task3.js
+++ task3.js
@@ -10,24 +10,50 @@ function post_time_content(){
 
     var text_box = document.getElementsByTagName('textarea')[0].value;
 
-    // Create elements for post time and content
-    var postTimeElement = document.createElement('div');
-    postTimeElement.classList.add('post-time'); // Adding class for post time
-    postTimeElement.style.fontWeight = 'bold'; // Styling as bold
-    postTimeElement.style.color = 'grey'; // Styling color as grey
-    postTimeElement.textContent = new Date();
+    //section 3.4
+    var postQuantity = document.querySelector('input[name="quantity"]').value;
 
-    var postContentElement = document.createElement('div');
-    postContentElement.classList.add('post-content'); // Adding class for post content
-    postContentElement.textContent = text_box;
+    var colour = ''; // Variable to store the selected color
 
-    // Append the elements to the posts div
-    var postsDiv = document.getElementById('posts');
+    // Check which color is selected
+    if(document.getElementsByName("color")[0].checked == true){
+        colour = 'blue';
+    } 
+    else if(document.getElementsByName("color")[1].checked == true){
+        colour = 'red';
+    }
+    else{
+        colour = 'grey';
+    }
+    
+    for(var i=0;i<postQuantity;i++){
 
-    postsDiv.appendChild(postTimeElement);
-    postsDiv.appendChild(document.createElement('br'));
-    postsDiv.appendChild(postContentElement);
-    postsDiv.appendChild(document.createElement('br'));
+        // Create elements for post time and content
+        var postTimeElement = document.createElement('div');
+        postTimeElement.classList.add('post-time'); // Adding class for post time
+        postTimeElement.style.fontWeight = 'bold'; // Styling as bold
+        postTimeElement.style.color = 'grey'; // Styling color as grey
+        postTimeElement.textContent = new Date();
+
+
+        var postContentElement = document.createElement('div');
+        postContentElement.classList.add('post-content'); // Adding class for post content
+        postContentElement.textContent = text_box;
+
+        // Append the elements to the posts div
+        var postsDiv = document.getElementById('posts');
+
+        // Apply color only to the new post
+        if(colour != ''){
+            postContentElement.style.color = colour;
+        }
+
+        postsDiv.appendChild(postTimeElement);
+        postsDiv.appendChild(document.createElement('br'));
+        postsDiv.appendChild(postContentElement);
+        postsDiv.appendChild(document.createElement('br'));
+
+    }
 }
 
 //section 3.3
@@ -46,5 +72,4 @@ function menu_hide_button() {
         menuDiv.style.display = 'none';
     }
 }
-
 
